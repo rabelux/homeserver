237a2e280348d6f294aa14e41dcf949e05734433
Alexander Raab <alexander.raab1@gmx.de>
2021-07-12 22:02:44 +0200

    Added third cmdline option to specify outname

--- Video-Editing/SubtitleReplace.sh
+++ Video-Editing/SubtitleReplace.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Prove two paths as argument. First path is the video-source, second is the subtitle-source. Both paths need same amount of files in mkv-format.
+# Prove two paths and the output-name-prefix as argument. First path is the video-source, second is the subtitle-source. Both paths need same amount of files in mkv-format.
+# Third argument is something like "GOT_S01E" and will end up in "GOT_S01Exx.mkv
 
 FILES=("$1"/*.mkv)
 SUB_SOURCES=("$2"/*.mkv)
@@ -18,7 +19,7 @@ if [ -f "${FILES[0]}" ]; then
 				*)		exit;;
 			esac
 		done
-		mkvmerge -o GOT_S03E$(printf "%02d" $((i+1))).mkv -S "${f}" --no-audio --no-video --no-chapters "${SUB_SOURCES[i]}"
+		mkvmerge -o "$3""$(printf "%02d" $((i+1)))".mkv -S "${f}" --no-audio --no-video --no-chapters "${SUB_SOURCES[i]}"
 		((i++))
 	done
 else
